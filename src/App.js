0a3767b0a9a5d127de7fc6394c28d84d3637bd26
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-05-31 00:28:08 +0700

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ImageSlider from "./components/ImageSlider";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSlider/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageData.js
@@ -0,0 +1,16 @@
+const ImageData = [
+    {
+        title:"เที่ยวทะเล",
+        image:"https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_1280.jpg",
+    },
+    {
+        title:"ดอกทานตะวัน",
+        image:"https://cdn.pixabay.com/photo/2018/07/20/13/52/sunflower-3550693_1280.jpg",
+    },
+    {
+        title:"ผีเสื้อ",
+        image:"https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_1280.jpg",
+    },
+]
+
+export default ImageData;
